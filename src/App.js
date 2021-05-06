0e41ea13daea2c468f315c8229f26e1957c8fdfa
David <dspark410@gmail.com>
2021-05-06 16:30:10 -0700

    adds styling

--- src/App.js
+++ src/App.js
@@ -1,154 +1,118 @@
 /** @format */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import './App.css'
-import {
-	BiMovie,
-	AiOutlineSearch,
-	BiRightArrow,
-	BiLeftArrow,
-} from 'react-icons/all'
-
-import Loader from './components/Loader'
-import Aos from 'aos'
 import 'aos/dist/aos.css'
-
+import './App.css'
+import Aos from 'aos'
+import Loader from './components/Loader'
 import Movies from './components/Movies'
 
 function App() {
-	const [input, setInput] = useState('')
-	const [movies, setMovies] = useState([])
-	const [page, setPage] = useState(1)
-	const [initial, setInitial] = useState(true)
-	const [loading, setLoading] = useState(true)
-	const [skeleton, setSkeleton] = useState(false)
+  const [input, setInput] = useState('')
+  const [movies, setMovies] = useState([])
+  const [page, setPage] = useState(1)
+  const [initial, setInitial] = useState(true)
+  const [loading, setLoading] = useState(true)
+  const [skeleton, setSkeleton] = useState(false)
 
-	const API_KEY = process.env.REACT_APP_API_KEY
+  const API_KEY = process.env.REACT_APP_API_KEY
+  const session = JSON.parse(sessionStorage.getItem('movie')) || ''
 
-	const session = JSON.parse(sessionStorage.getItem('movie')) || ''
+  const loadMovies = async () => {
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?s=dark&type=movie&page=1&apikey=${API_KEY}`
+    )
+    setMovies(data)
+  }
 
-	const loadMovies = async () => {
-		const { data } = await axios.get(
-			`https://www.omdbapi.com/?s=dark&type=movie&page=1&apikey=${API_KEY}`
-		)
-		setMovies(data)
-	}
+  const getMovies = async (page = 1) => {
+    if (input === '') {
+      const { data } = await axios.get(
+        `https://www.omdbapi.com/?s=${session.trim()}&type=movie&page=${page}&apikey=${API_KEY}`
+      )
+      setMovies(data)
+    } else {
+      const { data } = await axios.get(
+        `https://www.omdbapi.com/?s=${input.trim()}&type=movie&page=${page}&apikey=${API_KEY}`
+      )
+      setMovies(data)
+    }
+  }
 
-	const getMovies = async (page = 1) => {
-		if (input === '') {
-			const { data } = await axios.get(
-				`https://www.omdbapi.com/?s=${session.trim()}&type=movie&page=${page}&apikey=${API_KEY}`
-			)
-			setMovies(data)
-		} else {
-			const { data } = await axios.get(
-				`https://www.omdbapi.com/?s=${input.trim()}&type=movie&page=${page}&apikey=${API_KEY}`
-			)
-			setMovies(data)
-		}
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
+    if (input === '') return
 
-		if (input === '') return
+    sessionStorage.setItem('movie', JSON.stringify(input))
 
-		sessionStorage.setItem('movie', JSON.stringify(input))
+    setSkeleton(true)
+    setTimeout(() => {
+      getMovies()
+      setInput('')
+      setPage(1)
+      setInitial(false)
+      setSkeleton(false)
+    }, 1500)
+  }
 
-		setSkeleton(true)
-		setTimeout(() => {
-			getMovies()
-			setInput('')
-			setPage(1)
-			setInitial(false)
-			setSkeleton(false)
-		}, 1500)
-	}
+  const getPreviousPage = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1)
+    }
+  }
 
-	const getPreviousPage = () => {
-		if (page > 1) {
-			setPage((prev) => prev - 1)
-		}
-	}
+  const getNextPage = () => {
+    setPage((prev) => prev + 1)
+  }
 
-	const getNextPage = () => {
-		setPage((prev) => prev + 1)
-	}
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      loadMovies()
+      setLoading(false)
+      Aos.init({
+        duration: 1000,
+      })
+    }, 3000)
+    return () => {
+      clearTimeout(timer)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-	useEffect(() => {
-		let timer = setTimeout(() => {
-			loadMovies()
-			setLoading(false)
-			Aos.init({
-				duration: 1000,
-			})
-		}, 3000)
-		return () => {
-			clearTimeout(timer)
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
+  useEffect(() => {
+    let mounted = true
+    if (mounted && page > 0) {
+      getMovies(page)
+    }
+    return () => {
+      mounted = false
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page])
 
-	useEffect(() => {
-		let mounted = true
-		if (mounted && page > 0) {
-			getMovies(page)
-		}
-		return () => {
-			mounted = false
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [page])
+  if (loading) {
+    return <Loader />
+  } else {
+    return (
+      <div data-aos='fade' className='container'>
+        <h1>The Shoppies</h1>
 
-	if (loading) {
-		return <Loader />
-	} else {
-		return (
-			<div data-aos='fade' className='container'>
-				<h1>The Shoppies</h1>
-
-				<div className='text-container'>
-					<p>Nominate your favorite movies!</p>
-					<BiMovie className='movie-icon' />
-				</div>
-
-				<div className='form-container'>
-					<form onSubmit={handleSubmit}>
-						<input
-							spellCheck={false}
-							type='text'
-							placeholder='search movie title... e.g. "dark"'
-							value={input}
-							onChange={(e) => setInput(e.target.value)}
-						/>
-						<AiOutlineSearch onClick={handleSubmit} className='search-icon' />
-					</form>
-
-					{input !== '' && session
-						? ''
-						: initial
-						? ''
-						: movies.Search && (
-								<div className='button-container'>
-									<button
-										className={page === 1 ? 'disabled' : 'previous'}
-										disabled={page === 1}
-										onClick={getPreviousPage}>
-										<BiLeftArrow />
-									</button>
-									<button
-										className={movies.Search.length < 10 ? 'disabled' : 'next'}
-										disabled={movies.Search.length < 10}
-										onClick={getNextPage}>
-										<BiRightArrow />
-									</button>
-								</div>
-						  )}
-				</div>
-				<Movies movies={movies} skeleton={skeleton} />
-			</div>
-		)
-	}
+        <Movies
+          movies={movies}
+          skeleton={skeleton}
+          handleSubmit={handleSubmit}
+          input={input}
+          setInput={setInput}
+          initial={initial}
+          page={page}
+          getPreviousPage={getPreviousPage}
+          getNextPage={getNextPage}
+          session={session}
+        />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,136 +2,201 @@
 
 import React, { useState, useEffect } from 'react'
 import { imageLink } from '../utils/noPoster'
-import { MdGrade, AiOutlineClose } from 'react-icons/all'
+import {
+  MdGrade,
+  AiOutlineClose,
+  AiOutlineSearch,
+  BiRightArrow,
+  BiLeftArrow,
+  BiMovie,
+} from 'react-icons/all'
 import MoviesSkeleton from './MoviesSkeleton'
 
-function Movies({ movies, skeleton }) {
-	const [nominations, setNominations] = useState(
-		JSON.parse(localStorage.getItem('nominations')) || []
-	)
+function Movies({
+  movies,
+  skeleton,
+  handleSubmit,
+  input,
+  setInput,
+  initial,
+  page,
+  getPreviousPage,
+  getNextPage,
+  session,
+}) {
+  const [nominations, setNominations] = useState(
+    JSON.parse(localStorage.getItem('nominations')) || []
+  )
 
-	const addNomination = (title, year) => {
-		const nominationArr = [...nominations]
+  const addNomination = (title, year) => {
+    const nominationArr = [...nominations]
 
-		const duplicate = nominationArr
-			.map((nomination) => {
-				return nomination.title === title
-			})
-			.includes(true)
+    const duplicate = nominationArr
+      .map((nomination) => {
+        return nomination.title === title
+      })
+      .includes(true)
 
-		if (!duplicate && nominations.length < 5)
-			nominationArr.push({ title, year })
+    if (!duplicate && nominations.length < 5)
+      nominationArr.push({ title, year })
 
-		setNominations(nominationArr)
-	}
+    setNominations(nominationArr)
+  }
 
-	const removeNomination = (title, year) => {
-		const nominationArr = [...nominations]
-		const filteredArr = nominationArr.filter((nomination) => {
-			return title !== nomination.title || year !== nomination.year
-		})
-		setNominations(filteredArr)
-	}
+  const removeNomination = (title, year) => {
+    const nominationArr = [...nominations]
+    const filteredArr = nominationArr.filter((nomination) => {
+      return title !== nomination.title || year !== nomination.year
+    })
+    setNominations(filteredArr)
+  }
 
-	const getActiveClass = (title, year) => {
-		const nominationsArr = nominations.filter((nomination) => {
-			return nomination.title === title
-		})
-		if (nominationsArr[0] === undefined) {
-			return 'star-icon-inactive'
-		} else {
-			if (nominationsArr[0].title === title) {
-				return 'star-icon-active'
-			} else {
-				return 'star-icon-inactive'
-			}
-		}
-	}
+  const getActiveClass = (title) => {
+    const nominationsArr = nominations.filter((nomination) => {
+      return nomination.title === title
+    })
+    if (nominationsArr[0] === undefined) {
+      return 'star-icon-inactive'
+    } else {
+      if (nominationsArr[0].title === title) {
+        return 'star-icon-active'
+      } else {
+        return 'star-icon-inactive'
+      }
+    }
+  }
 
-	const getNominations = (title, year) => {
-		const nominationsArr = nominations.filter((nomination) => {
-			return nomination.title === title
-		})
-		if (nominationsArr[0] !== undefined) {
-			if (
-				nominationsArr[0].title === title &&
-				nominationsArr[0].year === year
-			) {
-				removeNomination(title, year)
-			}
-		} else {
-			addNomination(title, year)
-		}
-	}
+  const getNominations = (title, year) => {
+    const nominationsArr = nominations.filter((nomination) => {
+      return nomination.title === title
+    })
+    if (nominationsArr[0] !== undefined) {
+      if (
+        nominationsArr[0].title === title &&
+        nominationsArr[0].year === year
+      ) {
+        removeNomination(title, year)
+      }
+    } else {
+      addNomination(title, year)
+    }
+  }
 
-	useEffect(() => {
-		localStorage.setItem('nominations', JSON.stringify(nominations))
-	}, [nominations])
+  useEffect(() => {
+    localStorage.setItem('nominations', JSON.stringify(nominations))
+  }, [nominations])
 
-	return (
-		<>
-			<div className='nomination-container'>
-				<h1>Nomination List</h1>
+  return (
+    <>
+      <div className='form-nomination-container'>
+        <div className='text-form-container'>
+          <div className='text-container'>
+            <p>Nominate your favorite movies!</p>
+            <BiMovie className='movie-icon' />
+          </div>
+          <div className='form-container'>
+            <form onSubmit={handleSubmit}>
+              <input
+                spellCheck={false}
+                type='text'
+                placeholder='search movie title... e.g. "dark"'
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+              <AiOutlineSearch onClick={handleSubmit} className='search-icon' />
+            </form>
 
-				{nominations.map((nomination, i) => (
-					<div
-						key={i}
-						className={
-							nomination.title.length > 30
-								? 'long-nomination-title'
-								: 'nomination'
-						}>
-						<div key={i}>{`${nomination.title} (${nomination.year})`}</div>
-						<AiOutlineClose
-							className='close-icon'
-							onClick={() =>
-								removeNomination(nomination.title, nomination.year)
-							}
-						/>
-					</div>
-				))}
-			</div>
-			<div className='movies-container'>
-				{skeleton ? (
-					<MoviesSkeleton />
-				) : (
-					movies.Search &&
-					movies.Search.map((movie, i) => (
-						<div className='movies' key={i}>
-							<div className='movie-img-container'>
-								<img
-									className='movie-img'
-									src={movie.Poster !== 'N/A' ? movie.Poster : imageLink}
-									alt={movie.Title}
-								/>
+            {input !== '' && session
+              ? ''
+              : initial
+              ? ''
+              : movies.Search && (
+                  <div className='button-container'>
+                    <button
+                      className={page === 1 ? 'disabled' : 'previous'}
+                      disabled={page === 1}
+                      onClick={getPreviousPage}>
+                      <BiLeftArrow />
+                    </button>
+                    <button
+                      className={
+                        movies.Search.length < 10 ? 'disabled' : 'next'
+                      }
+                      disabled={movies.Search.length < 10}
+                      onClick={getNextPage}>
+                      <BiRightArrow />
+                    </button>
+                  </div>
+                )}
+          </div>
+        </div>
 
-								<MdGrade
-									className={getActiveClass(movie.Title)}
-									key={i}
-									onClick={() => getNominations(movie.Title, movie.Year)}
-								/>
-							</div>
-							<div className={movie.Title.length > 30 ? 'long-title' : 'title'}>
-								{movie.Title}
-							</div>
-							<div className='year'>{movie.Year}</div>
-						</div>
-					))
-				)}
-			</div>
+        <div className='nomination-container'>
+          <h2 className='nomination-header'>Nomination List</h2>
 
-			{movies.Error === 'Movie not found!' && (
-				<div className='no-movie'>
-					Movie not found. Please be more specific!
-				</div>
-			)}
-			{movies.Error === 'Too many results.' && (
-				<div className='too-many-movies'>
-					Too many results. Please be more specific!
-				</div>
-			)}
-		</>
-	)
+          {nominations.map((nomination, i) => (
+            <div key={i} className='nomination-box-container'>
+              <div
+                key={i}
+                className={
+                  nomination.title.length > 40
+                    ? 'long-nomination-title'
+                    : 'nomination'
+                }>
+                {`${i + 1}. ${nomination.title} (${nomination.year})`}
+              </div>
+              <AiOutlineClose
+                className='close-icon'
+                onClick={() =>
+                  removeNomination(nomination.title, nomination.year)
+                }
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='movies-container'>
+        {skeleton ? (
+          <MoviesSkeleton />
+        ) : (
+          movies.Search &&
+          movies.Search.map((movie, i) => (
+            <div className='movies' key={i}>
+              <div className='movie-img-container'>
+                <img
+                  className='movie-img'
+                  src={movie.Poster !== 'N/A' ? movie.Poster : imageLink}
+                  alt={movie.Title}
+                />
+
+                <MdGrade
+                  className={getActiveClass(movie.Title)}
+                  key={i}
+                  onClick={() => getNominations(movie.Title, movie.Year)}
+                />
+              </div>
+              <div className={movie.Title.length > 30 ? 'long-title' : 'title'}>
+                {movie.Title}
+              </div>
+              <div className='year'>{movie.Year}</div>
+            </div>
+          ))
+        )}
+      </div>
+
+      {movies.Error === 'Movie not found!' && (
+        <div className='no-movie'>
+          Movie not found. Please be more specific!
+        </div>
+      )}
+      {movies.Error === 'Too many results.' && (
+        <div className='too-many-movies'>
+          Too many results. Please be more specific!
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Movies
